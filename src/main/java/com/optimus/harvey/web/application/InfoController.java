abcc825e5e0339a102950c854eb3e3b0d6584a4b
bha1 <peeyushbhawan@gmail.com>
2017-08-29 00:17:08 +1000

    basic structural sanity restored

--- src/main/java/com/optimus/harvey/web/application/InfoController.java
+++ src/main/java/com/optimus/harvey/web/application/InfoController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/info")
+@RequestMapping("/information")
 public class InfoController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getInfo(Model model){
-		model.addAttribute("info","information");
-		return "info";
+	public String getInfo(@RequestParam(value="number", required=false)String numb,Model model){
+		model.addAttribute("information1","some text");
+		return "information";
 	}
 }
