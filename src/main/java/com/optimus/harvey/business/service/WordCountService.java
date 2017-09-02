86ac9196b6bd5715616a30678d230ecae3c54955
rogueagent <piyushbhawan@gmail.com>
2017-09-02 16:02:55 +1000

    1) added logger properties and logger file added to git ignore 2) added hash map for counting words

--- src/main/java/com/optimus/harvey/business/service/WordCountService.java
+++ src/main/java/com/optimus/harvey/business/service/WordCountService.java
@@ -1,12 +1,16 @@
 package com.optimus.harvey.business.service;
 
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WordCountService {
+	
+	private static final Logger logger = Logger.getLogger(WordCountService.class.getName());
 	
 	@Autowired
 	public WordCountService() {
@@ -15,11 +19,21 @@ public class WordCountService {
 	
 	public HashMap<String,Integer> countWords(String sourceString){
 		HashMap<String, Integer> wordMap = new HashMap<>();
+		try {
 		//write some logic here
-		wordMap.put("a", 2);
-		wordMap.put("b", 7);
-		wordMap.put("h", 1);
-		
+		String[] arrayOfStrings = sourceString.split("\\s");
+		if(arrayOfStrings!=null){
+		for(int i = 0; i<arrayOfStrings.length; i++) {
+				if(wordMap.containsKey((arrayOfStrings[i]).toLowerCase())) {
+					wordMap.put(arrayOfStrings[i].toLowerCase(), wordMap.get(arrayOfStrings[i])+1);
+				}else {
+					wordMap.put(arrayOfStrings[i].toLowerCase(), 1);
+				}
+			}
+		}
+		}catch(Exception e) {
+			logger.log(Level.SEVERE, "WTF !!!!", e);
+		}
 		return wordMap;
 	}
 
--- src/main/java/com/optimus/harvey/web/application/WordCountController.java
+++ src/main/java/com/optimus/harvey/web/application/WordCountController.java
@@ -6,8 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.optimus.harvey.business.domain.WordCount;
 import com.optimus.harvey.business.service.WordCountService;
+import com.optimus.harvey.business.domain.*;
 
 @Controller
 @RequestMapping("/wordcount")
